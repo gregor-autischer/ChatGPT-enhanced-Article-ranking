31fd32e674195e778613ffd93edad5baa5fb459e
Gregor Autischer <apps@autischer.me>
2024-12-10 15:11:20 +0100

    added chatgpt integration

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 ## Getting and preparing the Data
 
 ## ChatGPT integration
+Do this in your environment:
+To make the ChatGPT integration work:
+export OPENAI_API_KEY="your_api_key_here"
+
+## Using venv
+To create virtual environment the first time use:
+python -m venv env
+
+Activate env on windows:
+.\env\Scripts\activate
+
+Activate on Mac/Linux:
+source env/bin/activate
+
+To deactivate env:
+deactivate
+
+To install the requirements.txt run:
+pip install -r requirements.txt
---
+++ rank_articles.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import sys
+
+import openai
+from openai import OpenAI
+
+def main():
+    # Check if search term is passed
+    if len(sys.argv) > 1:
+        search_term_string = "".join(sys.argv[1:])
+    else:
+        print("No Search term passed!")
+        return
+    
+    print("Search Term:", search_term_string)
+
+    # extend search term with ChatGPT
+    additional_words_string = extend_search_term_with_gpt(search_term_string)
+
+    print("Additional Words from ChatGPT:")
+    print(additional_words_string)
+
+    search_term = search_term_string.split()
+    search_term_extended = additional_words_string.split()
+
+    print(search_term)
+    print(search_term_extended)
+
+def extend_search_term_with_gpt(search_term_string):
+    # ChatGPT integration
+
+    #gpt_system_content = "You are a helpful assistant."
+    gpt_system_content = "You find additional synonymes and words to improve an article search"
+    gpt_user_content = f"Give me additional words and synonyms that improve a keyword search for {search_term_string}. Just return between 5 and 15 words as a string with no bulletpoints or similar. Return only words no phrases."
+
+    try:
+        client = OpenAI()
+        completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+                {"role": "system", "content": gpt_system_content},
+                {"role": "user", "content": gpt_user_content}
+            ]
+        )
+        additional_words_string = completion.choices[0].message.content
+    
+    except openai.OpenAIError as e:
+        print(f"An OpenAI error occurred: {str(e)}")
+        return
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {str(e)}")
+        return
+
+    return additional_words_string
+
+
+
+if __name__ == "__main__":
+    main()
